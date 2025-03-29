12e8d4b82a1b556baea68cb69b7b973b89499981
vert5x-git <etotop2023@gmail.com>
2025-03-29 15:02:56 +0300

    Create MassDel.py

---
+++ MassDel.py
@@ -0,0 +1,26 @@
+from telethon.tl.functions.messages import SearchGlobal from telethon.tl.functions.messages import DeleteMessages from .. import loader, utils
+
+class MassDeleteMod(loader.Module): """Удаление всех ваших сообщений в группе"""
+
+strings = {
+    "name": "MassDelete",
+    "processing": "🚀 Удаление сообщений...",
+    "done": "✅ Удаление завершено!"
+}
+
+async def massdelcmd(self, message):
+    """Удаляет все ваши сообщения в текущем чате"""
+    chat = message.chat_id
+    me = await message.client.get_me()
+    
+    await utils.answer(message, self.strings["processing"])
+    
+    all_messages = []
+    async for msg in message.client.iter_messages(chat, from_user=me.id):
+        all_messages.append(msg.id)
+    
+    if all_messages:
+        await message.client(DeleteMessages(requests=all_messages, revoke=True))
+    
+    await utils.answer(message, self.strings["done"])
+
