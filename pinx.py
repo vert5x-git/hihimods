331d1b9540e0c4dcc6c7cf6d098c14b09dee6daf
vert5x-git <etotop2023@gmail.com>
2025-04-20 19:57:54 +0300

    Create pinx.py

---
+++ pinx.py
@@ -0,0 +1,34 @@
+# meta developer: @Vert5x
+# meta name: PingX
+# meta description: Проверка пинга и аптайма с фиксированным выводом
+
+from .. import loader
+import time
+import datetime
+
+@loader.tds
+class PingXMod(loader.Module):
+    """бесполезный кастомным пинг"""
+    strings = {"name": "PingX"}
+
+    def __init__(self):
+        self._start_time = time.time()
+
+    async def pinxcmd(self, message):
+        """Пинг и аптайм в стиле Matrix"""
+        start = time.time()
+        m = await message.edit("⏳ Пинг...")
+        ping = round((time.time() - start) * 1000)
+        uptime = str(datetime.timedelta(seconds=int(time.time() - self._start_time)))
+
+        me = await message.client.get_me()
+        owner_link = f'<a href="https://t.me/{me.username}">{me.first_name}</a>'
+
+        text = (
+            f"{owner_link}\n\n"
+            "<blockquote>🌩️ <b><i>𝚜𝚢𝚗𝚝𝚑𝚎𝚝𝚒𝚌 𝚛𝚎𝚏𝚕𝚎𝚌𝚝𝚒𝚘𝚗:</i></b> <code>{ping}</code> 𝚖𝚜</blockquote>\n"
+            "<blockquote>🧿 <b><i>𝚘𝚙𝚎𝚗 𝚎𝚢𝚎 𝚊𝚌𝚝𝚒𝚟𝚎 𝚜𝚒𝚗𝚌𝚎:</i></b> <code>{uptime}</code></blockquote>\n"
+            "<blockquote>🌌 <b><i>𝚕𝚒𝚗𝚔 𝚝𝚘 𝚖𝚊𝚝𝚛𝚒𝚡 𝚌𝚘𝚗𝚏𝚒𝚛𝚖𝚎𝚍.</i></b></blockquote>"
+        ).format(ping=ping, uptime=uptime)
+
+        await m.edit(text)
