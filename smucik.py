652a783f9b5b54698563658a41ae69944ea5b4be
vert5x-git <etotop2023@gmail.com>
2025-05-05 10:37:56 +0600

    Update smucik.py

--- smucik.py
+++ smucik.py
@@ -1,11 +1,10 @@
 # meta developer: @shprot03
 # meta name: SMusicTrack
-# meta version: 1.0
+# meta version: 1.1
 
 from .. import loader, utils
-from telethon.tl.functions.messages import CreateChatRequest, DeleteChatUserRequest, AddChatUserRequest
-from telethon.tl.functions.messages import GetMessagesRequest
-from telethon.tl.types import InputUser, InputPeerUser
+from telethon.tl.functions.messages import CreateChatRequest, DeleteChatUserRequest
+from telethon.tl.functions.channels import LeaveChannelRequest
 import asyncio
 
 class SMusicTrackMod(loader.Module):
@@ -21,25 +20,29 @@ class SMusicTrackMod(loader.Module):
 
         try:
             # Создание временной группы
-            group = await message.client(CreateChatRequest(users=[smusic], title="TempSMusicGroup"))
+            chat = await message.client(CreateChatRequest(users=[smusic], title="TempSMusicGroup"))
+            group_id = chat.chats[0].id
 
             await asyncio.sleep(1)
 
-            # Отправка запроса в группу
-            await message.client.send_message(group.chats[0].id, query)
+            # Отправка запроса
+            await message.client.send_message(group_id, query)
+            await message.edit("Ожидаю ответ...")
 
-            await asyncio.sleep(4)
+            await asyncio.sleep(5)
 
-            async for msg in message.client.iter_messages(group.chats[0].id, from_user_id=smusic.id, limit=5):
-                if msg.audio or msg.document:
+            async for msg in message.client.iter_messages(group_id, limit=10):
+                if msg.sender_id == smusic.id and (msg.audio or msg.document):
                     await msg.forward_to(message.chat_id)
                     break
             else:
                 await message.edit("Трек не найден.")
                 return
 
-            await message.client(DeleteChatUserRequest(group.chats[0].id, smusic.id))
-            await message.client(DeleteChatUserRequest(group.chats[0].id, me.id))
+            await asyncio.sleep(1)
+            await message.client(DeleteChatUserRequest(group_id, smusic.id))
+            await asyncio.sleep(1)
+            await message.client(LeaveChannelRequest(group_id))
 
         except Exception as e:
             await message.edit(f"Ошибка: {e}")
